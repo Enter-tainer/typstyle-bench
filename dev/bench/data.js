9c3443bba46b32592a45cba2c70c2d0e57f6b2e7
github-action-benchmark <github@users.noreply.github.com>
2024-11-29 16:24:48 +0000

    add Rust Benchmark (cargo) benchmark result for 6aa542c6cbbec45350bc486a1a80b5d662d42746

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1732896811479,
+  "lastUpdate": 1732897488156,
   "repoUrl": "https://github.com/Enter-tainer/typstyle",
   "entries": {
     "Rust Benchmark": [
@@ -239,6 +239,126 @@ window.BENCHMARK_DATA = {
             "name": "pretty-packages/touying/utils",
             "value": 1210026,
             "range": "± 10862",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mgt@oi-wiki.org",
+            "name": "mgt",
+            "username": "Enter-tainer"
+          },
+          "committer": {
+            "email": "mgt@oi-wiki.org",
+            "name": "Wenzhuo Liu",
+            "username": "Enter-tainer"
+          },
+          "distinct": true,
+          "id": "6aa542c6cbbec45350bc486a1a80b5d662d42746",
+          "message": "chore: bump to v0.12.5",
+          "timestamp": "2024-11-30T00:20:11+08:00",
+          "tree_id": "b3cc0c568422f1930c9802b65a044223b263728c",
+          "url": "https://github.com/Enter-tainer/typstyle/commit/6aa542c6cbbec45350bc486a1a80b5d662d42746"
+        },
+        "date": 1732897487776,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "attrs-tablex",
+            "value": 657997,
+            "range": "± 8841",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "attrs-cetz-manual",
+            "value": 117224,
+            "range": "± 1772",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "attrs-undergraduate-math",
+            "value": 179412,
+            "range": "± 1941",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "attrs-packages/codly",
+            "value": 360233,
+            "range": "± 5647",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "attrs-packages/fletcher-diagram",
+            "value": 70008,
+            "range": "± 611",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "attrs-packages/fletcher-draw",
+            "value": 226884,
+            "range": "± 1705",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "attrs-packages/touying/core",
+            "value": 441100,
+            "range": "± 2173",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "attrs-packages/touying/utils",
+            "value": 224044,
+            "range": "± 824",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "pretty-tablex",
+            "value": 3383571,
+            "range": "± 50603",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "pretty-cetz-manual",
+            "value": 912785,
+            "range": "± 5437",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "pretty-undergraduate-math",
+            "value": 976943,
+            "range": "± 76683",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "pretty-packages/codly",
+            "value": 1744945,
+            "range": "± 8028",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "pretty-packages/fletcher-diagram",
+            "value": 517506,
+            "range": "± 4342",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "pretty-packages/fletcher-draw",
+            "value": 1210615,
+            "range": "± 7477",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "pretty-packages/touying/core",
+            "value": 2157616,
+            "range": "± 16392",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "pretty-packages/touying/utils",
+            "value": 1207992,
+            "range": "± 26952",
             "unit": "ns/iter"
           }
         ]
